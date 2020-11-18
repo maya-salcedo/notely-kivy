b2f978859f1fa65f38a907ebcbcc255de78257d1
maya <maya.salcedo07@gmail.com>
2020-11-18 21:48:50 +0200

    added some functions

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from os.path import join, exists
 import json
-import kivy.app import App
+from kivy.app import App
 from kivy.uix.screenmanager import Screen, ScreenManager, SlideTransition
 from kivy.clock import Clock
 from kivy.core.window import Window
@@ -12,20 +12,82 @@ from kivy.properties import ListProperty, StringProperty, NumericProperty, Boole
 Window.borderless = True
 
 class NoteView(Screen):
-    pass
+    note_index: NumericProperty()
+    note_title: StringProperty()
+    note_content: StringProperty()
 
 class NoteListItem(BoxLayout):
-    pass
+    note_index: NumericProperty()
+    note_title: StringProperty()
+    note_content: StringProperty()
 
 class Notes(Screen):
-    pass
+    data = ListProperty()
+
+    def _get_data_for_widgets(self):
+        return [{
+            "note_index": index,
+            "note_content": item["content"],
+            "note_title": item["title"]}
+            for index, item in enumerate(self.data)]
+
+    data_for_widgets = AliasProperty(_get_data_for_widgets,bind= ["data"])
 
 class MutableTextInput(FloatLayout):
     pass
 
 class MainApp(App):
-    pass
+
+    def build(self):
+        self.notes = Notes(name="notes")
+        root = ScreenManager()
+        root.add_widget(self.notes)
+        return root
+
+    def go_notes(self):
+        self.transition.direction = "right"
+        self.root.current = "notes"
+
+    def save_notes(self):
+        with open(self.notes_fn, "w") as fd:
+            json.dump(self.notes.data, fd)
+
+    def refresh_notes(self):
+        data = self.notes.data
+        self.notes.data = []
+        self.notes.data = data
+
+    def del_note(self, note_index):
+        del self.notes.data[note_index]
+        self.save_notes()
+        self.refresh_notes()
+        self.go_notes()
+
+    def edit_note(self, note_index):
+        note = self.notes.data[note_index]
+        name = "notes{}".format(note_index)
+
+        if self.root.has_screen(name):
+            self.root.remove_widget(self.root.get_screen(name))
+
+        view = NoteView(
+            name = name,
+            note_index = note_index,
+            note_title = note.get("title")
+            note_content = note.get("content"))
+        self.root.add_widget(view)
+        self.transition.direction = "left"
+        self.root.current = view.name
+
+    def add_note(self):
+        self.notes.data.append({"title": "New note", "content": ""})
+        note_index = len(self.notes.data)-1
+        self.edit_note(note_index)
 
 
+    @property
+    def note_fn(self):
+        return join(self.user_data_dir, "notes.json")
 
-MainApp().run()
+if __name__ == "__main__":
+    MainApp().run()
